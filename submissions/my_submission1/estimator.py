a75c4493c4575827e6243d578e3ff2cb7323d5c4
acktan <94125871+acktan@users.noreply.github.com>
2021-11-27 16:46:15 +0100

    27/11/21 16:46

--- submissions/my_submission1/estimator.py
+++ submissions/my_submission1/estimator.py
@@ -3,17 +3,13 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import sklearn
-import arrow
-import dateutil.parser
 import math
-from sklearn.preprocessing import FunctionTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, OrdinalEncoder, FunctionTransformer
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import Ridge
 from sklearn.ensemble import HistGradientBoostingRegressor
+
+__file__ = Path('submissions') /  'external_data' /  'estimator.py'
 
 def _cycl_encode(X):
     X = X.copy()
@@ -36,23 +32,53 @@ def _cycl_encode(X):
     return X.drop(columns=["date"]) 
 
 
+def _merge_external_data(X):
+    file_path = Path(__file__).parent / 'external_data.csv'
+    df_ext = pd.read_csv(file_path, parse_dates=['date'])
+    X = X.copy()
+    # When using merge_asof left frame need to be sorted
+    X['orig_index'] = np.arange(X.shape[0])
+    X = pd.merge_asof(X.sort_values('date'), df_ext[['date', 't', 'ff', 'u', 'brent', 'vacances_scol', 'bank_hol']].sort_values('date'), on='date')
+    # Sort back to the original order
+    X = X.sort_values('orig_index')
+    del X['orig_index']
+    return X
+
+def _encode_dates(X):
+    X = X.copy()  # modify a copy of X
+    # Encode the date information from the DateOfDeparture columns
+    X.loc[:, 'year'] = X['date'].dt.year
+    X.loc[:, 'month'] = X['date'].dt.month
+    X.loc[:, 'day'] = X['date'].dt.day
+    X.loc[:, 'weekday'] = X['date'].dt.weekday
+    X.loc[:, 'hour'] = X['date'].dt.hour
+
+    return X.drop(columns=["date"]) 
+
+    # Finally we can drop the original columns from the dataframe
 
 def get_estimator():
-    date_encoder = FunctionTransformer(_cycl_encode)
-    date_cols = ['year_sin', 'year_cos', 'month_sin', 'month_cos', 'day_sin', 'day_cos', 'weekday_sin', 'weekday_cos', 'hour_sin', 'hour_cos']
+    #date_encoder = FunctionTransformer(_encode_dates)
+    #date_cols = ['year', 'month', 'day', 'weekday', 'hour']
 
-    categorical_encoder = OneHotEncoder(handle_unknown="ignore")
-    categorical_cols = ["counter_name", "site_name"]
-    numeric_cols = ['t', 'ff', 'u']
+    cycl_encoder = FunctionTransformer(_cycl_encode)
+    cycl_cols = ['year_sin', 'year_cos', 'month_sin', 'month_cos', 'day_sin', 'day_cos', 'weekday_sin', 'weekday_cos', 'hour_sin', 'hour_cos']
+
+    categorical_encoder = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
+    categorical_cols = ["site_name", "counter_name"]
+
+    numeric_cols = ['t', 'ff', 'u', 'brent', 'bank_hol', 'vacances_scol']
 
     preprocessor = ColumnTransformer(
         [
-            ("date", "passthrough", date_cols),
-            ("cat", categorical_encoder, categorical_cols)
+            ('date', 'passthrough', cycl_cols),
+            ('cat', categorical_encoder, categorical_cols),
+            ('numeric', 'passthrough', numeric_cols)
         ]
     )
     regressor = HistGradientBoostingRegressor(random_state=0)
 
-    pipe = make_pipeline(date_encoder, preprocessor, regressor)
+    pipe = make_pipeline(
+        FunctionTransformer(_merge_external_data, validate=False), cycl_encoder, preprocessor, regressor)
 
     return pipe
